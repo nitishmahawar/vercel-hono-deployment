380d61b2b70a02ed43b4e0da7c27b4621a35ed32
Nitish Mahawar <nitishmahawar.dev@gmail.com>
2025-08-02 13:32:18 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/vercel", (c) => {
+  return c.json({ message: "Hello Vercel!" });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
+
+export default app;
